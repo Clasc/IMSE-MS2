3453bab7de28ba94773951af65b7082707e19a26
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-26 07:40:06 +0100

    DataService (+) add files from other repo

---
+++ Dataservice/src/Router.ts
@@ -0,0 +1,14 @@
+
+import {Express} from "express";
+export class Router{
+    private readonly app: Express;
+
+    constructor(app: Express){
+        this.app = app;
+        this.app.get('/', (req, res)=> res.send("Hello World"));
+    }
+
+    public createRoutes(){
+        this.app.get("/test", (req, res)=> {res.send("This is the test subpage");});
+    }
+}
---
+++ Dataservice/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import {Router} from "./Router"
+const app = express();
+const port = 3000;
+
+const router = new Router(app);
+router.createRoutes();
+
+app.listen(port, ()=>{
+    console.log(`REST API running on Port ${port}`);
+});
