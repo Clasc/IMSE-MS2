5d989a3a29174e4c56e5efed6c0533fb160f8ce1
Christian Lascsak <christian.lascsak7@gmail.com>
2021-01-03 14:43:55 +0100

    add tsc --watch and npm run dev to package json

--- Dataservice/src/app.ts
+++ Dataservice/src/app.ts
@@ -1,14 +1,12 @@
 import express from "express";
 import { Router } from "./Router"
-import { db } from "./Services/db";
+
 const app = express();
 const port = 3000;
 
 const router = new Router(app);
 router.createRoutes();
 
-
-db.config;
 
 app.listen(port, () => {
     console.log(`REST API running on Port ${port}`);
