b2b25418c64bec6feb0fdc3798d3ae3f489e450b
Yvonne-Nadine Krivanek <a01404589@unet.univie.ac.at>
2021-01-06 18:13:15 +0100

    insert Rebos and Controllers for all tables

--- Dataservice/src/Router.ts
+++ Dataservice/src/Router.ts
@@ -1,6 +1,12 @@
 
 import { Express } from "express";
 import { deleteUser, getAllUsers, getUserById, insertUser } from "./Controllers/UserController";
+import { insertStudio, getAllStudios } from "./Controllers/StudioController";
+import { insertGame, getAllGames } from "./Controllers/GameController";
+import { insertGameRecommendation, getAllGameRecommendations } from "./Controllers/GameRecommendationController";
+import { insertRent, getAllRents } from "./Controllers/RentController";
+import { insertSubscription, getAllSubscriptions } from "./Controllers/SubscriptionController";
+import { insertPlayedGame, getAllPlayedGames } from "./Controllers/PlayedGameController";
 
 export class Router {
     private readonly app: Express;
@@ -15,6 +21,32 @@ export class Router {
         this.app.get("/users", getAllUsers);
         this.app.get("/users/:userId", getUserById);
         this.app.put("/users/:userId", insertUser);
+        this.app.put("/users/", insertUser);
         this.app.delete("/users/:userId", deleteUser);
+
+        this.app.get("/studios", getAllStudios);
+        this.app.put("/studios/:studioId", insertStudio);
+        this.app.put("/studios/", insertStudio);
+
+        this.app.get("/games", getAllGames);
+        this.app.put("/games/:gameId", insertGame);
+        this.app.put("/games/", insertGame);
+
+        this.app.get("/gameRecommendations", getAllGameRecommendations);
+        this.app.put("/gameRecommendations/:gameRecommendationId", insertGameRecommendation);
+        this.app.put("/gameRecommendations/", insertGameRecommendation);
+
+        this.app.arguments("/rents", getAllRents);
+        this.app.put("/rents/:rentId", insertRent);
+        this.app.put("/rents/", insertRent);
+
+        this.app.get("/subscriptions", getAllSubscriptions);
+        this.app.put("/subscriptions/:subscriptionId", insertSubscription);
+        this.app.put("/subscriptions/", insertSubscription);
+
+        this.app.get("/playedGames", getAllPlayedGames);
+        this.app.put("/playedGames/_playedGameId", insertPlayedGame);
+        this.app.put("/playedGames/", insertPlayedGame);
+
     }
 }
