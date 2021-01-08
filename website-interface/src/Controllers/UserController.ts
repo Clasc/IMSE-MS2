4f56372c78a7672700874002ca28a86a5791917a
Christian Lascsak <christian.lascsak7@gmail.com>
2021-01-08 17:35:03 +0100

    login (c) hash user password before save

--- website-interface/src/Controllers/UserController.ts
+++ website-interface/src/Controllers/UserController.ts
@@ -1,3 +1,4 @@
+import { createHash } from "crypto";
 import { Request, Response } from "express";
 import { User } from "../Dtos/User";
 
@@ -7,22 +8,23 @@ import { UserApiService } from "../Services/UserApiService";
 export async function registerUser(req: Request, res: Response) {
     let user = new User();
     if (!req.body) {
-        res.status(500).send(`Request body is empty`);
+        res.status(400).send(`Request body is empty`);
         return
     }
 
-    user.username = req.body?.username;
-    user.first_name = req.body?.first_name;
-    user.last_name = req.body?.last_name;
-    user.password = req.body?.password;
-    user.birthday = req.body?.birthday;
-
-    let errorMessage = await validateUserData(user);
+    let errorMessage = await validateUserData(req.body as User);
 
     if (errorMessage) {
         res.status(200).send({ success: false, error: errorMessage });
         return;
     }
+
+    user.username = req.body?.username;
+    user.first_name = req.body?.first_name;
+    user.last_name = req.body?.last_name;
+    let hash = createHash("md5");
+    user.password = hash.update(req.body.password).digest('hex');
+    user.birthday = req.body?.birthday;
 
     let success = await UserApiService.insertUser(user);
     res.status(success ? 200 : 500).send({ success: success });
@@ -36,12 +38,9 @@ async function validateUserData(user: User): Promise<string | null> {
     errorMessage = user.birthday ? null : "birthday is a required field";
 
     let users = await UserApiService.getUserByUsername(user.username);
-    console.log("userapi users:", users);
     if (users && users.length > 0) {
         return "This username already exists";
     }
-
-    errorMessage = users.length > 0 ? "birthday is a required field" : "";
 
     let currentYear = new Date().getFullYear();
     let birthDate = new Date(user.birthday);
