f70f2121ffd42af92170783aad861db3020c780e
Christian Lascsak <christian.lascsak7@gmail.com>
2021-01-10 11:57:29 +0100

    add retrying connection to db

--- Dataservice/src/Services/db.ts
+++ Dataservice/src/Services/db.ts
@@ -1,5 +1,4 @@
 import { createConnection, MysqlError } from "mysql";
-import { User } from "../Dtos/User";
 
 
 let db = createConnection({
@@ -10,10 +9,36 @@ let db = createConnection({
     port: Number.parseInt(process.env.MYSQL_PORT ?? "3306")
 });
 
-db.connect((err: MysqlError) => {
-    if (err) throw err;
-    console.log("Connected to database!");
-});
+let maxRetries = process.env.DB_CONNECT_RETRY ? parseInt(process.env.DB_CONNECT_RETRY) : 5;
+let retryTimeout = process.env.DB_CONNECT_TIMEOUT ? parseInt(process.env.DB_CONNECT_TIMEOUT) : 500;
+
+tryConnect();
+
+async function tryConnect(retries: number = 1) {
+    if (retries > maxRetries) {
+        throw "Could not connect to Database.";
+    }
+
+    try {
+        await connectDb();
+        console.log("Connected to db!");
+    }
+    catch (err) {
+        console.log("Error connecting to db, retrying", err);
+        await setTimeout(async () => await tryConnect(++retries), retryTimeout);
+    }
+}
+
+async function connectDb(): Promise<void> {
+    new Promise<void>((resolve, reject) => db.connect((err: MysqlError) => {
+        if (err) {
+            reject(err);
+        }
+        else {
+            resolve();
+        }
+    }));
+}
 
 export async function queryDb(query: string, values?: any): Promise<[any]> {
     return new Promise((resolve, reject) => db.connect(() => {
