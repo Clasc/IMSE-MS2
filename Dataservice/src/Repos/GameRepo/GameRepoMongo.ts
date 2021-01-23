66404dea5929ca492aa8bc51b2063438066376f6
Christian Lascsak <christian.lascsak7@gmail.com>
2021-01-23 17:30:24 +0100

    mongodb (+) add functions for GameRepoMongo

--- Dataservice/src/Repos/GameRepo/GameRepoMongo.ts
+++ Dataservice/src/Repos/GameRepo/GameRepoMongo.ts
@@ -6,15 +6,21 @@ import { IGameRepo } from "./IGameRepo";
 export class GameRepoMongo implements IGameRepo {
 
     public async getAllGames(): Promise<Game[]> {
-        mongoDB.createCollection("GameRepotest");
-        return [new Game()];
+        return mongoDB.collection("Game").find().toArray();
     }
 
     public async getGameById(id: string): Promise<Game | null> {
-        throw new Error("Method not implemented.");
+        return (await mongoDB.collection("Game").find({ game_id: parseInt(id) }).toArray())[0];
     }
 
     public async insertGame(game: Game): Promise<boolean> {
-        throw new Error("Method not implemented.");
+        try {
+            await mongoDB.collection("Game").insertOne(game);
+            return true;
+        }
+        catch (err) {
+            console.error(err);
+            return false
+        };
     }
 }
