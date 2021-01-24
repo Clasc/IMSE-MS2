20941e8089ca6825fd91c8a0565d58e24846ae8d
Yvonne-Nadine Krivanek <a01404589@unet.univie.ac.at>
2021-01-24 16:58:39 +0100

    fix dates for mongo repo

--- Dataservice/src/Dtos/Rent/Rent.ts
+++ Dataservice/src/Dtos/Rent/Rent.ts
@@ -1,7 +1,7 @@
 export interface Rent {
     rent_id?: number;
     extended?: boolean;
-    start_date?: string;
-    expiration_date?: string;
+    start_date?: string | Date;
+    expiration_date?: string | Date;
     user_id?: number;
 }
