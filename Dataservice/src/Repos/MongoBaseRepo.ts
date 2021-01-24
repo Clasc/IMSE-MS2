8f4b0efd5d245e7903212e00aafce24585ad93c0
Christian Lascsak <christian.lascsak7@gmail.com>
2021-01-24 10:34:02 +0100

    mongodb (-) remove console.log from increment

--- Dataservice/src/Repos/MongoBaseRepo.ts
+++ Dataservice/src/Repos/MongoBaseRepo.ts
@@ -5,7 +5,6 @@ export class MongoBaseRepo {
         let sortFunction: any = {};
         sortFunction[field.idField] = -1;
         let values = await mongoDB.collection(field.collecition).find().sort(sortFunction).limit(1).toArray();
-        console.log("max values:", values)
         if (!values[0] || !values[0][field.idField] || values[0][field.idField] == 0) {
             return 0;
         }
