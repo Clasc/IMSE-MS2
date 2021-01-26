b81353f4efe3674d8c22fc36d6f2ea2c595dd359
Christian Lascsak <christian.lascsak7@gmail.com>
2021-01-26 18:50:11 +0100

    update indexes for Rent and User

--- db/mongo-setup/mongo-setup.js
+++ db/mongo-setup/mongo-setup.js
@@ -1,13 +1,13 @@
 let res = [
 
-  db.User.createIndex({ user_id: 1 }, { unique: true }),
+  db.User.createIndex({ user_id: 1, 'played_games.game.game_id': 1 }),
   db.User.createIndex({ username: 1 }),
 
   db.Game.createIndex({ game_id: 1 }, { unique: true }),
 
   db.Rent.createIndex({ rent_id: 1 }, { unique: true }),
-  // db.Rent.createIndex("ExtendIndex",{user_id:1, game_id:1, expiration_date:1}),
-  // db.Rent.createIndex("ReportIndex",{user_id:1, start_date:1}),
+  db.Rent.createIndex({ user_id: 1, "game.game_id": 1, expiration_date: 1 }),
+  db.Rent.createIndex({ start_date: 1 }),
 
   db.Subscription.createIndex({ subscription_id: 1 }, { unique: true }),
 
